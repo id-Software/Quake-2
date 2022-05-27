f4932af99bd30484d839a4d12bdfe59735c4cbc9
muxyrmuc <muxyrmuc@gmail.com>
2022-05-27 22:07:15 +0300

    remove unused crc.h

--- qcommon/crc.h
+++
@@ -1,6 +0,0 @@
-/* crc.h */
-
-void CRC_Init(unsigned short *crcvalue);
-void CRC_ProcessByte(unsigned short *crcvalue, byte data);
-unsigned short CRC_Value(unsigned short crcvalue);
-unsigned short CRC_Block (byte *start, int count);
