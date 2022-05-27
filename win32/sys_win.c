55c813cf211e73b4f01991d844eab915fec58b45
muxyrmuc <muxyrmuc@gmail.com>
2022-05-27 21:55:41 +0300

    remove unused resource.h

--- win32/sys_win.c
+++ win32/sys_win.c
@@ -21,7 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "../qcommon/qcommon.h"
 #include "winquake.h"
-#include "resource.h"
 #include <errno.h>
 #include <float.h>
 #include <fcntl.h>
